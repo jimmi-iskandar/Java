646e77a859a8b7aa17d4a7024c84214cfc120a72
jimmi-iskandar <jim.iskandar20@gmail.com>
2023-04-07 18:15:27 +0700

    panel

--- OOP/src/com/frame/login.java
+++ OOP/src/com/frame/login.java
@@ -2,17 +2,20 @@ package com.frame;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.Color;
 
 public class login {
     public static void log() {
         JFrame windows = new JFrame();
         JPanel background = new JPanel();
 
-        windows.setSize(600, 300);
+        windows.setSize(600, 400);
         windows.setTitle("Login");
-        background.setSize(600, 300);
+        background.setSize(600, 400);
+        background.setBackground(Color.BLUE);
 
         windows.setVisible(true);
+        windows.add(background);
         windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
